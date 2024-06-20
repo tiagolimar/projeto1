0ef1c2377530e00c691d9063a3237cc2081c4851
TIAGO LIMA SOUSA <TIAGO0341@DIGITALCOLLEGE.LOCAL>
2024-06-20 19:29:01 -0300

    add files

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+setInterval(displayTime, 1000);
+
+function displayTime() {
+    const display = document.getElementsByTagName('h3')[0];
+    const date = new Date();
+    display.innerHTML = date.toLocaleString();    
+}
