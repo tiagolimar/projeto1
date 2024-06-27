a856d0d5eae1a4046f3efe6ea2b25ab54ac49679
BRUNO <BRUNO6053@DIGITALCOLLEGE.LOCAL>
2024-06-27 19:07:52 -0300

    alterando para LocaleString

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function mostrarDataHora() {
     const date = new Date();
-    console.log(date.toDateString());
+    console.log(date.toLocaleString());
     
     document.getElementById('date').innerText = date.toLocaleString();
 }
